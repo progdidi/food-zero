82d3c379d7b7e4e9d64fa60e44455b3b89cbccff
Diana Sosnovskaya <progdidi@gmail.com>
2023-09-27 12:49:42 +0200

    feat: add contacts page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
-import MainPage from "./pages/MainPage/MainPage";
 import Footer from "./components/Footer/Footer";
+import MainPage from "./pages/MainPage/MainPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
 import MenuPage from "./pages/MenuPage/MenuPage";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Header/>
         <main className="main">
           {/* <MainPage/> */}
-          <MenuPage/>
+          {/* <MenuPage/> */}
+          <ContactsPage/>
         </main>      
       <Footer/>
     </div>
---
+++ src/pages/ContactsPage/ContactsPage.js
@@ -0,0 +1,45 @@
+//components
+import PagePromo from "../../components/PagePromo/PagePromo";
+import ReservationSection from "../../components/ReservationSection/ReservationSection";
+
+//images
+import banner from './images/banner.png';
+import contacts1 from './images/contacts1.png';
+import contacts2 from './images/contacts2.png';
+import reservBg from './images/reservBg.svg';
+
+import './contactsPage.scss';
+
+const ContactsPage = () => {
+    return ( 
+        <>
+            <PagePromo title="Get in Touch" descr="The freshest ingredients for you every day" bgImage={banner}/>
+            
+            <div className="contacts">
+                <div className="container">
+                    <div className="contacts__item">
+                        <img src={contacts1} alt="" className="contacts__item-img" />
+                        <div className="contacts__item-info">
+                            <p className="contacts__item-text">We can be contacted via <br /> email <a href="mail:info@foodzero.com" className="contacts__item-link">info@foodzero.com </a> <br/>
+                            or telephone on <a href="tel:86852346000" className="contacts__item-link">+86 852 346 000</a>
+                            </p>
+                        </div>
+                        
+                    </div>
+                    <div className="contacts__item"> 
+                        <div className="contacts__item-info">
+                            <p className="contacts__item-text">We are located in 1959 Sepulveda Blvd. Culver City, CA, 90230</p>
+                            <button className="contacts__item-btn main-btn">View in maps</button>
+                        </div>                       
+                       
+                        <img src={contacts2} alt="" className="contacts__item-img" />
+                    </div>
+                </div>
+            </div>
+
+            <ReservationSection/>
+        </>
+     );
+}
+ 
+export default ContactsPage;

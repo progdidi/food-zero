742bce83645b96aa118b723f113db0e8aef2fb5f
Diana Sosnovskaya <progdidi@gmail.com>
2023-09-30 23:49:15 +0200

    responsive
    blog item component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Header/>
+        <Header/>
           <main className="main">
             <Routes>
               <Route path="/" element={<MainPage/>} />
@@ -27,7 +27,7 @@ function App() {
               <Route path="/blog" element={<BlogPage/>} />
               <Route path="/article" element={<BlogArticlePage/>} />
             </Routes>
-          </main>       */}
+          </main>      
         <Footer/>
       </div>
 
